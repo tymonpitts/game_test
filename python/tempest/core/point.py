e6f68b54d3505ec8729c3e254c9412bc2a3fe00c
Tymon Pitts <tpitts@wetafx.co.nz>
2016-01-27 19:42:16 +1300

    Added Point.distance()

--- python/tempest/core/point.py
+++ python/tempest/core/point.py
@@ -1,14 +1,12 @@
+from .abstract_vector import AbstractVector
+import numpy
+
 __all__ = ['Point']
 
-#============================================================================#
-#================================================================= IMPORTS ==#
-
-from .abstract_vector import AbstractVector
-
-#============================================================================#
-#=================================================================== CLASS ==#
 class Point(AbstractVector):
     @property
     def w(self):
         return 1.0
 
+    def distance(self, other):
+        return numpy.linalg.norm(self - other)
