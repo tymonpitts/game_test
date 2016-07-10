f4ca92e819decaf8b7d8884de1b3d80a2c02ab50
Tymon Pitts <tpitts@wetafx.co.nz>
2016-07-10 14:29:13 +1200

    Updated cache_method decorator to construct the cache variable's name itself rather than it being passed into the decorator

--- python/tempest/core/decorators.py
+++ python/tempest/core/decorators.py
@@ -10,7 +10,8 @@ class EnableCachedMethods(type):
     The created method will be called by the decorator at runtime.
     See the docs for `cached_method` for more info.
     """
-    def __new__(cls, name, bases, cls_dict):
+    @staticmethod
+    def _add_cached_method(cls_dict):
         for obj_name, obj in cls_dict.items():
             if not inspect.isfunction(obj):
                 continue
@@ -18,13 +19,16 @@ class EnableCachedMethods(type):
                 continue
 
             code = inspect.cleandoc('''
-                def _cached__%(func_name)s(self):
-                    return self.%(cache_variable_name)s
-            ''') % dict(func_name=obj_name, cache_variable_name=obj.cache_variable_name)
+                def _%(func_name)s__cached(self):
+                    return self._%(func_name)s__result
+            ''') % dict(func_name=obj_name)
             exec code in cls_dict
+
+    def __new__(cls, name, bases, cls_dict):
+        cls._add_cached_method(cls_dict)
         return type.__new__(cls, name, bases, cls_dict)
 
-def cached_method(cache_variable_name):  # type: types.FunctionType
+def cached_method(func):  # type: types.FunctionType
     """ This is a function decorator that, when used in conjunction with the
     `EnableCachedMethods` metaclass, sets up caching on a method so that
     subsequent calls to it will be faster.
@@ -32,49 +36,38 @@ def cached_method(cache_variable_name):  # type: types.FunctionType
     Requirements:
         - The method must have no arguments other than the `self` argument.
 
-    Args:
-        cache_variable_name (str):
-            The name of the variable to use to cache the result of the method.
-
     Returns:
         types.FunctionType
     """
-    # assert that this decorator was called with the appropriate arguments
+    # assert that the function has the required argspec
     #
-    assert isinstance(cache_variable_name, basestring), 'cached_method decorator expects 1 argument specifying the name of the cache variable'
+    argspec = inspect.getargspec(func)
+    assert len(argspec.args) == 1 and argspec.args[0] == 'self', 'Invalid arg spec for decorated function.  Expected no arguments.'
 
-    def wrapper(func):
-        # assert that the function has the required argspec
-        #
-        argspec = inspect.getargspec(func)
-        assert len(argspec.args) == 1 and argspec.args[0] == 'self', 'Invalid arg spec for decorated function.  Expected no arguments.'
+    # Wrap `func` by:
+    # - assigning the result to the variable named by `cache_variable_name`
+    # - re-assigning func's name to a method that just returns the cache result.
+    #   This method will be created by the `EnableCachedMethods` metaclass.
+    # - returning the cached result
+    code = inspect.cleandoc('''
+        @functools.wraps(func)
+        def wrapped(self):
+            self._%(func_name)s__result = func(self)
+            self.%(func_name)s = self._%(func_name)s__cached
+            return self._%(func_name)s__result
+    ''') % dict(func_name=func.__name__)
+    wrapped_func_container = {
+        'functools': functools,
+        'func': func,
+    }
+    exec code in wrapped_func_container
 
-        # Wrap `func` by:
-        # - assigning the result to the variable named by `cache_variable_name`
-        # - re-assigning func's name to a method that just returns the cache result.
-        #   This method will be created by the `EnableCachedMethods` metaclass.
-        # - returning the cached result
-        code = inspect.cleandoc('''
-            @functools.wraps(func)
-            def wrapped(self):
-                self.%(cache_variable_name)s = func(self)
-                self.%(func_name)s = self._cached__%(func_name)s
-                return self.%(cache_variable_name)s
-        ''') % dict(cache_variable_name=cache_variable_name, func_name=func.__name__)
-        wrapped_func_container = {
-            'functools': functools,
-            'func': func,
-        }
-        exec code in wrapped_func_container
+    # Add some attributes to the wrapped function created above.
+    # These attributes are required by the `EnableCachedMethods` metaclass
+    # later so that it can create a method that just returns the cached result of `func`.
+    #
+    wrapped = wrapped_func_container['wrapped']
+    wrapped.cached_method = True
 
-        # Add some attributes to the wrapped function created above.
-        # These attributes are required by the `EnableCachedMethods` metaclass
-        # later so that it can create a method that just returns the cached result of `func`.
-        #
-        wrapped = wrapped_func_container['wrapped']
-        wrapped.cached_method = True
-        wrapped.cache_variable_name = cache_variable_name
-
-        return wrapped
-    return wrapper
+    return wrapped
 
--- tests/test_core/test_decorators.py
+++ tests/test_core/test_decorators.py
@@ -2,13 +2,13 @@ import pytest
 from tempest.core import decorators
 
 def test_cached_method():
-    # assert that the cached_method decorator errors if no arguments are passed in
+    # assert that the cached_method decorator errors if the decorated method has arguments
     with pytest.raises(AssertionError):
         class FailureClass(object):
             __metaclass__ = decorators.EnableCachedMethods
 
             @decorators.cached_method
-            def failure_method(self):
+            def failure_method(self, unexpected_arg):
                 pass
 
     class Foo(object):
@@ -16,18 +16,20 @@ def test_cached_method():
 
         count = 0
 
-        @decorators.cached_method('_cached_method_result')
-        def cached_method(self):
+        @decorators.cached_method
+        def bar(self):
             Foo.count += 1
             return Foo.count
 
+    assert hasattr(Foo, '_bar__cached')
+
     inst1 = Foo()
     inst2 = Foo()
-    assert inst1.cached_method() == inst1.cached_method()
-    assert inst2.cached_method() == inst2.cached_method()
-    assert inst1.cached_method() != inst2.cached_method()
+    assert inst1.bar() == inst1.bar()
+    assert inst2.bar() == inst2.bar()
+    assert inst1.bar() != inst2.bar()
 
-    assert hasattr(inst1, '_cached_method_result')
-    assert inst1._cached_method_result == 1
-    assert inst1.cached_method.__name__ == '_cached__cached_method'
+    assert hasattr(inst1, '_bar__result')
+    assert inst1._bar__result == 1
+    assert inst1.bar.__name__ == '_bar__cached'
 
