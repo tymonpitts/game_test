1c5b7edbb38b1d8f087f4e4fa142a1460b8add7c
Tymon Pitts <tpitts@wetafx.co.nz>
2016-08-20 19:00:54 +1200

    Added tests for AbstractTree

---
+++ tests/test_core/test_tree.py
@@ -0,0 +1,49 @@
+from tempest import core
+import unittest
+
+class TreeTestCase(unittest.TestCase):
+    def setUp(self):
+        data = [
+            [0, 1, 2, 3],
+            [4, 5, 6, 7],
+            [8, 9, 10, 11],
+            [12, 13, 14, 15],
+        ]
+        self.tree = core.QuadTree(8, 3)
+        self.tree._root = data
+
+    def test_init(self):
+        assert self.tree.size == 8.0
+        assert self.tree.max_depth == 3
+        assert self.tree.min_size == 1.0
+
+    def test_get_node(self):
+        # check depth=0
+        assert self.tree.get_node(core.Point())._data is self.tree._root
+
+        # check depth=1
+        assert self.tree.get_node(core.Point(-2.0, -2.0))._data is self.tree._root[0]
+        assert self.tree.get_node(core.Point( 2.0, -2.0))._data is self.tree._root[1]
+        assert self.tree.get_node(core.Point(-2.0,  2.0))._data is self.tree._root[2]
+        assert self.tree.get_node(core.Point( 2.0,  2.0))._data is self.tree._root[3]
+
+        # check depth=2
+        assert self.tree.get_node(core.Point(-3.0, -3.0))._data is self.tree._root[0][0]
+        assert self.tree.get_node(core.Point(-1.0, -3.0))._data is self.tree._root[0][1]
+        assert self.tree.get_node(core.Point(-3.0, -1.0))._data is self.tree._root[0][2]
+        assert self.tree.get_node(core.Point(-1.0, -1.0))._data is self.tree._root[0][3]
+
+        assert self.tree.get_node(core.Point(1.0, -3.0))._data is self.tree._root[1][0]
+        assert self.tree.get_node(core.Point(3.0, -3.0))._data is self.tree._root[1][1]
+        assert self.tree.get_node(core.Point(1.0, -1.0))._data is self.tree._root[1][2]
+        assert self.tree.get_node(core.Point(3.0, -1.0))._data is self.tree._root[1][3]
+
+        assert self.tree.get_node(core.Point(-3.0, 1.0))._data is self.tree._root[2][0]
+        assert self.tree.get_node(core.Point(-1.0, 1.0))._data is self.tree._root[2][1]
+        assert self.tree.get_node(core.Point(-3.0, 3.0))._data is self.tree._root[2][2]
+        assert self.tree.get_node(core.Point(-1.0, 3.0))._data is self.tree._root[2][3]
+
+        assert self.tree.get_node(core.Point(1.0, 1.0))._data is self.tree._root[3][0]
+        assert self.tree.get_node(core.Point(3.0, 1.0))._data is self.tree._root[3][1]
+        assert self.tree.get_node(core.Point(1.0, 3.0))._data is self.tree._root[3][2]
+        assert self.tree.get_node(core.Point(3.0, 3.0))._data is self.tree._root[3][3]
