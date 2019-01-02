3a3c060659b600fbdeab0b978c9fd8caab5fce43
Tymon Pitts <tpitts@wetafx.co.nz>
2019-01-02 22:16:13 +1300

    WIP: updated smooth cube indexes to match Octree child indexes

--- python/tempest/data/lod_transition_proof_of_concept/smooth_cube.py
+++ python/tempest/data/lod_transition_proof_of_concept/smooth_cube.py
@@ -4,61 +4,60 @@ from OpenGL import GL
 
 
 VERTICES = (
-    #
-    #        7      4
+    # Vert indexes which match those of an Octree's child indexes
+    #        2      3
     #                     ^
-    #   3      0          y
+    #   6      7          y
     #                    z x >
-    #        6      5   L
-    #
-    #   2      1
-    #
-     0.5,  0.5,  0.5,
-     0.5, -0.5,  0.5,
-    -0.5, -0.5,  0.5,
-    -0.5,  0.5,  0.5,
-     0.5,  0.5, -0.5,
-     0.5, -0.5, -0.5,
+    #        0      1   L
+    #   
+    #   4      5
     -0.5, -0.5, -0.5,
+     0.5, -0.5, -0.5,
     -0.5,  0.5, -0.5,
+     0.5,  0.5, -0.5,
+    -0.5, -0.5,  0.5,
+     0.5, -0.5,  0.5,
+    -0.5,  0.5,  0.5,
+     0.5,  0.5,  0.5,
 )
 
 # normalized side for 45 degree normal
 normalized_45 = math.sqrt(pow(0.5, 2) * 3)
 
 NORMALS = (
-     normalized_45,  normalized_45,  normalized_45,
-     normalized_45, -normalized_45,  normalized_45,
-    -normalized_45, -normalized_45,  normalized_45,
-    -normalized_45,  normalized_45,  normalized_45,
-     normalized_45,  normalized_45, -normalized_45,
-     normalized_45, -normalized_45, -normalized_45,
     -normalized_45, -normalized_45, -normalized_45,
+     normalized_45, -normalized_45, -normalized_45,
     -normalized_45,  normalized_45, -normalized_45,
+     normalized_45,  normalized_45, -normalized_45,
+    -normalized_45, -normalized_45,  normalized_45,
+     normalized_45, -normalized_45,  normalized_45,
+    -normalized_45,  normalized_45,  normalized_45,
+     normalized_45,  normalized_45,  normalized_45,
 )
 DRAW_METHOD = GL.GL_TRIANGLES
 INDICES = (
     # FRONT FACE
-    0, 1, 2,
-    2, 3, 0,
+    4, 6, 5,
+    6, 7, 5,
 
     # TOP FACE
-    0, 3, 7,
-    7, 4, 0,
+    2, 3, 7,
+    7, 6, 2,
 
     # RIGHT FACE
+    1, 5, 7,
+    7, 3, 1,
+
+    # BACK FACE
+    0, 1, 2,
+    2, 1, 3,
+
+    # BOTTOM FACE
     0, 5, 1,
     0, 4, 5,
 
-    # BACK FACE
-    4, 6, 5,
-    4, 7, 6,
-
-    # BOTTOM FACE
-    1, 5, 6,
-    1, 6, 2,
-
     # LEFT FACE
-    2, 6, 3,
-    3, 6, 7,
+    0, 6, 4,
+    0, 2, 6,
 )
